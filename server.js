a28890413b76214179000c2a16f78971c1481fc8
Mattias Ljungmark <ljungmark@xpider.io>
2017-02-19 12:48:01 +0100

    Abstracting date retrieval

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-let express = require('express');
-let app = express();
-let path = require('path');
+const express = require('express'),
+  app = express(),
+  path = require('path');
 
-let mysql = require('mysql');
-let pool  = mysql.createPool({
+const mysql = require('mysql');
+const pool  = mysql.createPool({
 	connectionLimit: 10,
 	host: '',
 	user: '',
@@ -12,30 +12,53 @@ let pool  = mysql.createPool({
 	dateStrings: true
 });
 
+function get_date(date) {
+  current_date = new Date(),
+  year = current_date.getFullYear(),
+  month = ('0' + (current_date.getMonth() + 1)).slice(-2),
+  day = ('0' + current_date.getDate()).slice(-2),
+  date = year + '-' + month + '-' + day;
+}
+
+/** ROUTING */
 app.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.post('/synchronize', function(request, response) {
-	const current_date = new Date(),
-		year = current_date.getFullYear(),
-		month = ('0' + (current_date.getMonth() + 1)).slice(-2),
-		day = ('0' + current_date.getDate()).slice(-2),
-		date = year + '-' + month + '-' + day;
+/**
+  Synchronize the application with the database
+  Retrives the current application status from the database
+  Returns JSON encoded object with availability and possibly a stringified datetime.
 
-		console.log(date);
+  Example
+  {
+    'available': false,
+    'date': '2017-02-17 09:12:09'
+  }
+*/
+app.post('/synchronize', function(request, response) {
+	const date = get_date();
 
 	let sql = 'SELECT date FROM cookings WHERE DATE(date) = ?',
-		values = ['2017-02-17'];
+		values = [date];
 	sql = mysql.format(sql, values);
 
 	pool.query(sql, function (error, results, fields) {
     if (error) throw error;
-		// If there isn't a result
+
+		/**
+      If there isn't a result, it means that the cooking hasn't commenced yet
+      The app is availaboe for additional orders
+    */
 		if (!results.length) {
       response.send(JSON.stringify({ 'available': true }));
     } else {
-		  const return_values = Object.assign({ 'available': false }, results[0]);
+    /**
+      Else the cooking has already commenced
+      No futher orders will be accepted
+      Returns the datetime when the cooking started to process further actions
+    */
+      const return_values = Object.assign({ 'available': false }, results[0]);
 		  response.send(JSON.stringify(return_values));
     }
 	});
