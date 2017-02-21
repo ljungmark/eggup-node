c8c5c9f6428203ef8e87c8d91961d097ce061f55
Mattias Ljungmark <ljngmrk@gmail.com>
2017-02-21 08:18:00 +0100

    Add block load animations

--- static/eggup.js
+++ static/eggup.js
@@ -1,18 +1,58 @@
-document.addEventListener('DOMContentLoaded', function() {
+/** Helper functions */
 
-  /** Helper functions */
-  function serialize(object) {
-    let str = [];
+/**
+  onCSSAnimationEnd, trigger callback when CSS animation has ended on a element
+  onCSSTransitionEnd, trigger callback when CSS transition has ended on a element
+*/
+;( function ( document, window, index ) {
+  var s = document.body || document.documentElement, s = s.style, prefixAnimation = '', prefixTransition = '';
 
-    for (let property in object) {
-      if (object.hasOwnProperty(property)) {
-        str.push(encodeURIComponent(property) + "=" + encodeURIComponent(object[property]));
-      }
+  if( s.WebkitAnimation == '' ) prefixAnimation  = '-webkit-';
+  if( s.MozAnimation == '' )    prefixAnimation  = '-moz-';
+  if( s.OAnimation == '' )    prefixAnimation  = '-o-';
+
+  if( s.WebkitTransition == '' )  prefixTransition = '-webkit-';
+  if( s.MozTransition == '' )   prefixTransition = '-moz-';
+  if( s.OTransition == '' )   prefixTransition = '-o-';
+
+  Object.prototype.onCSSAnimationEnd = function( callback )
+  {
+    var runOnce = function( e ){ callback(); e.target.removeEventListener( e.type, runOnce ); };
+    this.addEventListener( 'webkitAnimationEnd', runOnce );
+    this.addEventListener( 'mozAnimationEnd', runOnce );
+    this.addEventListener( 'oAnimationEnd', runOnce );
+    this.addEventListener( 'oanimationend', runOnce );
+    this.addEventListener( 'animationend', runOnce );
+    if( ( prefixAnimation == '' && !( 'animation' in s ) ) || getComputedStyle( this )[ prefixAnimation + 'animation-duration' ] == '0s' ) callback();
+    return this;
+  };
+
+  Object.prototype.onCSSTransitionEnd = function( callback )
+  {
+    var runOnce = function( e ){ callback(); e.target.removeEventListener( e.type, runOnce ); };
+    this.addEventListener( 'webkitTransitionEnd', runOnce );
+    this.addEventListener( 'mozTransitionEnd', runOnce );
+    this.addEventListener( 'oTransitionEnd', runOnce );
+    this.addEventListener( 'transitionend', runOnce );
+    this.addEventListener( 'transitionend', runOnce );
+    if( ( prefixTransition == '' && !( 'transition' in s ) ) || getComputedStyle( this )[ prefixTransition + 'transition-duration' ] == '0s' ) callback();
+    return this;
+  };
+}(document, window, 0));
+
+function serialize(object) {
+  let str = [];
+
+  for (let property in object) {
+    if (object.hasOwnProperty(property)) {
+      str.push(encodeURIComponent(property) + "=" + encodeURIComponent(object[property]));
     }
-
-    return str.join("&");
   }
 
+  return str.join("&");
+}
+
+document.addEventListener('DOMContentLoaded', function() {
 
   let Eggup = function() {
 
@@ -86,10 +126,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
   Eggup.prototype.load = function(target_block) {
+    function whichTransitionEvent(){
+      var t;
+      var el = document.createElement('fakeelement');
+      var transitions = {
+        'transition':'transitionend',
+        'OTransition':'oTransitionEnd',
+        'MozTransition':'transitionend',
+        'WebkitTransition':'webkitTransitionEnd'
+      }
+
+      for(t in transitions){
+        if( el.style[t] !== undefined ){
+          return transitions[t];
+        }
+      }
+    }
+    var transitionEvent = whichTransitionEvent();
+
     let current_block = this.block;
 
     /** Allowed targets */
-    let block_array = [
+    const block_array = [
       'loading',
       'order',
       'observe',
@@ -97,8 +155,11 @@ document.addEventListener('DOMContentLoaded', function() {
       'closed'
     ];
 
-    current_block_index = block_array.indexOf(current_block);
-    target_block_index = block_array.indexOf(target_block);
+
+    const current_block_element = document.querySelector('.application__block--' + current_block),
+      target_block_element = document.querySelector('.application__block--' + target_block),
+      current_block_index = block_array.indexOf(current_block),
+      target_block_index = block_array.indexOf(target_block);
 
     /** If target is the same as current block */
     if (target_block_index == current_block_index) return false;
@@ -108,12 +169,35 @@ document.addEventListener('DOMContentLoaded', function() {
 
     /** Decide animatin direction */
     if (current_block_index < target_block_index) {
-      document.querySelector('.application__block--' + current_block).classList.add('application__block--hidden');
-      document.querySelector('.application__block--' + target_block).classList.remove('application__block--hidden');
+
+      current_block_element.classList.add('fade_out_to_left');
+      target_block_element.classList.remove('application__block--hidden');
+      target_block_element.classList.add('fade_in_from_right');
+
+      current_block_element.onCSSAnimationEnd( function()
+      {
+        current_block_element.classList.remove('fade_out_to_left');
+        current_block_element.classList.add('application__block--hidden');
+        target_block_element.classList.remove('fade_in_from_right');
+        console.log('hejsan');
+      });
+
       console.log('moving forward');
     } else {
-      document.querySelector('.application__block--' + current_block).classList.add('application__block--hidden');
-      document.querySelector('.application__block--' + target_block).classList.remove('application__block--hidden');
+
+      current_block_element.classList.add('fade_out_to_right');
+      target_block_element.classList.remove('application__block--hidden');
+      target_block_element.classList.add('fade_in_from_left');
+
+      current_block_element.onCSSAnimationEnd( function()
+      {
+        current_block_element.classList.remove('fade_out_to_right');
+        current_block_element.classList.add('application__block--hidden');
+        target_block_element.classList.remove('fade_in_from_left');
+        console.log('hejsan2');
+      });
+
+
       console.log('moving backwards');
     }
 
