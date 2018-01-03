0c6a5a90d615aa464c0b7e60cbde5f6ad00ad9bc
ljungmark <mattias@widence.com>
2018-01-03 21:10:12 +0100

    Correct Q36

--- static/eggup.js
+++ static/eggup.js
@@ -1041,7 +1041,7 @@ Eggup.prototype.i18n = function(operation = 'get', pointer = null) {
             '33': '"Måste vi heta vad vi gör? För jag sitter på Reddit här...." -Qaniel',
             '34': '"Jobbbar du som spets-slutet?" -Emilio',
             '35': '"Thihihihihi" -Andreas',
-            '36': '"Martin luktar som min rollspelsgrupp på 90-talet..." -Hübsch',
+            '36': '"Martin luktar som min rollspelsgrupp från 90-talet..." -Hübsch',
           }
         },
         'map': {
