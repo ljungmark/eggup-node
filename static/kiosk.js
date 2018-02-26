afa611e14564f3c8a1882d03c111321f64beaca6
ljungmark <mattias@widence.com>
2018-02-26 03:51:30 +0100

    Kiosk send request

--- static/kiosk.js
+++ static/kiosk.js
@@ -1,3 +1,20 @@
 
 document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('keydown', (event) => {
+    document.querySelector('.tag').focus();
+  })
+
+  document.querySelector('.form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', _ => document.querySelector('.tag').value = '');
+    xhr.open('POST', '/request');
+
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    const data = `tag=${document.querySelector('.tag').value}`;
+
+    xhr.send(data);
+  })
 });
