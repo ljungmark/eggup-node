da9573ac889a3d6f060591337df2e9afe0b93a10
ljungmark <mattias@widence.com>
2018-03-04 00:59:57 +0100

    view queue

--- static/kiosk.js
+++ static/kiosk.js
@@ -4,13 +4,36 @@ let ui_refresher;
 const templates = {
   'default': `<h1>Order eggs</h1>
     <p>Hold your tag in front of the reader</p>`,
-  'thank_you': `<h1>Order received</h1>
+  'inserted': `<h1>Order received</h1>
+    <p>Thank you! You can cancel your order in the app.</p>`,
+  'updated': `<h1>Order updated</h1>
     <p>Thank you! You can cancel your order in the app.</p>`,
   'tag_not_found': `<h1>Oh noes!</h1>
-    <p>Your tag hasn't been registered. Please contact Mattias.</p>`,
+    <p>Your tag hasn't been registered.</p>`,
   'have_a_great_day': `<h1>Have a great day!</h1>`,
   'terminal_closed': `<h1>Terminal is closed</h1>
     <p>Welcome back next work day</p>`,
+}
+
+function switchmessages(input = 'default', revert = false) {
+  document.querySelector('.queue').innerHTML = templates[input];
+
+  const direction = (input === 'default') ? 'downwards' : 'upwards';
+
+  document.querySelector('body').classList.add(direction);
+  setTimeout(function(){
+    document.querySelector('.view').innerHTML = document.querySelector('.queue').innerHTML;
+    document.querySelector('.queue').innerHTML = '';
+    document.querySelector('body').classList.remove(direction);
+  }, 300);
+
+  if (revert) {
+    ui_refresher = setTimeout(function(){
+      switchmessages();
+    }, 3000);
+  }
+
+  return false;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -29,28 +52,9 @@ document.addEventListener('DOMContentLoaded', function() {
       document.querySelector('.tag').value = '';
 
       const response = JSON.parse(request.target.response);
-      if (response.status === true) {
-        document.querySelector('.ui').innerHTML = templates.thank_you;
-
-        ui_refresher = setTimeout(function(){
-          document.querySelector('.ui').innerHTML = templates.default;
-        }, 3000);
-      } else {
-        if (response.data === 'tag_not_found') {
-          document.querySelector('.ui').innerHTML = templates.tag_not_found;
-
-          ui_refresher = setTimeout(function(){
-            document.querySelector('.ui').innerHTML = templates.default;
-          }, 3000);
-        } else if (response.data === 'terminal_closed') {
-          document.querySelector('.ui').innerHTML = templates.terminal_closed;
-
-          ui_refresher = setTimeout(function(){
-            document.querySelector('.ui').innerHTML = templates.default;
-          }, 3000);
-        }
-      }
+      switchmessages(response.data, response.status);
     });
+
     xhr.open('POST', '/request');
 
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
