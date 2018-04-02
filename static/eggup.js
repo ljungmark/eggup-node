92fd971a1722936f5ff665ddc30f8e58b1c0a86e
ljungmark <mattias@widence.com>
2018-04-02 16:25:41 +0200

    Q54

--- static/eggup.js
+++ static/eggup.js
@@ -1086,6 +1086,7 @@ Eggup.prototype.i18n = function(operation = 'get', pointer = null) {
             '51': '"Det är som att ha två rövhål istället för öron" -Marie',
             '52': '"Knark och genmodifikation är helt okej!" -Linus',
             '53': '"Vi måste göra slut på det här med är göra grejer som inte fungerar" -Linus',
+            '54': '"Nu vet jag varför man ska äta mat... Annars blir man ju värsta trött" -Marie',
           }
         },
         'map': {
