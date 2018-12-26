0939d51ad6a64a488313f1c171ae34920ae820e7
Mattias Ljungmark <ljngmrk@gmail.com>
2018-12-26 16:06:14 +0100

    Q110

--- static/eggup.js
+++ static/eggup.js
@@ -1168,6 +1168,7 @@ Eggup.prototype.i18n = function(operation = 'get', pointer = null) {
             '107': '"DECIMAL(24,12)...? Oh. My. God..." -Matthew',
             '108': '"It might be a bad idea, or it might be a good idea - but we have no idea...." -Qaniel',
             '109': '"Är deadlinen om 6 månader?! Vad gör  vi här?" -Boya',
+            '110': '"Stress? Embrace it... Or die!" -Wykes',
           }
         },
         'map': {
