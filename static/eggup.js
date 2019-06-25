20a6b7fee710ccf9f22cf0e0e1a7ee487953276b
Mattias Ljungmark <mattias@ljungmark.co>
2019-06-25 16:22:29 +0200

    Q143

--- static/eggup.js
+++ static/eggup.js
@@ -1201,6 +1201,7 @@ Eggup.prototype.i18n = function(operation = 'get', pointer = null) {
             '140': '"Är det stora bokstäver på siffrorna?" -Kvarnström',
             '141': '"Du ska ge fan i att kolla i koden" -Frisell',
             '142': '"Suck... Jag orkar inte med Java längre..." -Freij',
+            '143': '"I can hook you up with that... Hint hint!" - Roland',
           }
         },
         'map': {
