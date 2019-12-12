35a134320a071febf88ed01d9cc7e234db05ec01
Mattias Ljungmark <mattias@ljungmark.co>
2019-12-12 16:16:56 +0100

    Q154-156

--- static/eggup.js
+++ static/eggup.js
@@ -1182,7 +1182,9 @@ Eggup.prototype.i18n = function(operation = 'get', pointer = null) {
             '151': '"Ja men dåså! Då ska jag uppmärksamma att Freij är här" -Qaniel',
             '152': '""Vad fan är det här? Hur ska jag... Hur fan äter man det här?" -Rebecka',
             '153': '"Netflix är fan överallt. Om vi går ut och köper en brödrost så måste vi avinstallera Netflix först." -Qaniel',
-            '154': 'It\'s so hard with LDV... Because, you know... Numbers. -Emil',
+            '154': '"Java..." -Ljungmark',
+            '155': '"Hitta på något annat..." -Corbett',
+            '156': '"Släng ut mig ur chatten..." -Corbett',
           }
         },
         'map': {
