7b6be9032d9946603cc5986b61e8111802844a38
Mattias Ljungmark <mattias@ljungmark.co>
2019-12-21 09:20:12 +0100

    Babel compile kiosk

---
+++ static/kiosk-modern.js
@@ -0,0 +1,81 @@
+var socket = io();
+
+let ui_refresher;
+const templates = {
+  'default': `<h1>Order eggs</h1>
+    <p>Hold your tag in front of the reader</p>`,
+  'inserted': `<h1>Order confirmed</h1>
+    <p>Thank you and have a great day!</p>`,
+  'updated': `<h1>Order already placed</h1>
+    <p>If you wanted to cancel your order, you can do so in the app.</p>`,
+  'tag_not_found': `<h1>Oh noes!</h1>
+    <p>Your tag hasn't been registered. Please use the app!</p>
+    <p>Ref: {{tag_id}}</p>`,
+  'have_a_great_day': `<h1>You're great!</h1>`,
+  'closed_terminal': `<h1>Terminal is closed</h1>
+    <p>Welcome back next work day</p>`,
+  'too_large_quantity': `<h1>Invalid params</h1>
+    <p>Please provide valid input</p>`,
+}
+
+function switchmessages(input = 'default', revert = false, replacements = []) {
+  document.querySelector('.queue').innerHTML = templates[input].replace(/{{(.*?)}}/g, replacements[0]);
+
+  const direction = (input === 'default') ? 'downwards' : 'upwards';
+
+  document.querySelector('body').classList.add(direction);
+  setTimeout(function(){
+    document.querySelector('.view').innerHTML = document.querySelector('.queue').innerHTML;
+    document.querySelector('.queue').innerHTML = '';
+    document.querySelector('body').classList.remove(direction);
+  }, 300);
+
+  if (revert) {
+    ui_refresher = setTimeout(function(){
+      switchmessages();
+    }, 3000);
+  }
+
+  return false;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.tag').focus();
+
+  document.addEventListener('keydown', (event) => {
+    document.querySelector('.tag').focus();
+  })
+
+  document.querySelector('.form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', (request) => {
+      clearTimeout(ui_refresher);
+      document.querySelector('.tag').value = '';
+
+      socket.emit('heap');
+
+      const response = JSON.parse(request.target.response);
+      switchmessages(response.data, true, [response.tag_id]);
+    });
+
+    xhr.open('POST', '/request');
+
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    const data = `tag=${document.querySelector('.tag').value}`;
+
+    xhr.send(data);
+  });
+
+  document.querySelector('.tag').focus();
+});
+
+socket.on('gateway', function(open) {
+  if (open == false) {
+    switchmessages('have_a_great_day');
+  } else {
+    switchmessages();
+  }
+});
--- static/kiosk.js
+++ static/kiosk.js
@@ -1,37 +1,32 @@
+"use strict";
+
 var socket = io();
+var ui_refresher;
+var templates = {
+  'default': "<h1>Order eggs</h1>\n    <p>Hold your tag in front of the reader</p>",
+  'inserted': "<h1>Order confirmed</h1>\n    <p>Thank you and have a great day!</p>",
+  'updated': "<h1>Order already placed</h1>\n    <p>If you wanted to cancel your order, you can do so in the app.</p>",
+  'tag_not_found': "<h1>Oh noes!</h1>\n    <p>Your tag hasn't been registered. Please use the app!</p>\n    <p>Ref: {{tag_id}}</p>",
+  'have_a_great_day': "<h1>You're great!</h1>",
+  'closed_terminal': "<h1>Terminal is closed</h1>\n    <p>Welcome back next work day</p>",
+  'too_large_quantity': "<h1>Invalid params</h1>\n    <p>Please provide valid input</p>"
+};
 
-let ui_refresher;
-const templates = {
-  'default': `<h1>Order eggs</h1>
-    <p>Hold your tag in front of the reader</p>`,
-  'inserted': `<h1>Order confirmed</h1>
-    <p>Thank you and have a great day!</p>`,
-  'updated': `<h1>Order already placed</h1>
-    <p>If you wanted to cancel your order, you can do so in the app.</p>`,
-  'tag_not_found': `<h1>Oh noes!</h1>
-    <p>Your tag hasn't been registered. Please use the app!</p>
-    <p>Ref: {{tag_id}}</p>`,
-  'have_a_great_day': `<h1>You're great!</h1>`,
-  'closed_terminal': `<h1>Terminal is closed</h1>
-    <p>Welcome back next work day</p>`,
-  'too_large_quantity': `<h1>Invalid params</h1>
-    <p>Please provide valid input</p>`,
-}
-
-function switchmessages(input = 'default', revert = false, replacements = []) {
+function switchmessages() {
+  var input = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'default';
+  var revert = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+  var replacements = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
   document.querySelector('.queue').innerHTML = templates[input].replace(/{{(.*?)}}/g, replacements[0]);
-
-  const direction = (input === 'default') ? 'downwards' : 'upwards';
-
+  var direction = input === 'default' ? 'downwards' : 'upwards';
   document.querySelector('body').classList.add(direction);
-  setTimeout(function(){
+  setTimeout(function () {
     document.querySelector('.view').innerHTML = document.querySelector('.queue').innerHTML;
     document.querySelector('.queue').innerHTML = '';
     document.querySelector('body').classList.remove(direction);
   }, 300);
 
   if (revert) {
-    ui_refresher = setTimeout(function(){
+    ui_refresher = setTimeout(function () {
       switchmessages();
     }, 3000);
   }
@@ -39,40 +34,29 @@ function switchmessages(input = 'default', revert = false, replacements = []) {
   return false;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('.tag').focus();
-
-  document.addEventListener('keydown', (event) => {
+  document.addEventListener('keydown', function (event) {
     document.querySelector('.tag').focus();
-  })
-
-  document.querySelector('.form').addEventListener('submit', (event) => {
+  });
+  document.querySelector('.form').addEventListener('submit', function (event) {
     event.preventDefault();
-
-    const xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', (request) => {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', function (request) {
       clearTimeout(ui_refresher);
       document.querySelector('.tag').value = '';
-
       socket.emit('heap');
-
-      const response = JSON.parse(request.target.response);
+      var response = JSON.parse(request.target.response);
       switchmessages(response.data, true, [response.tag_id]);
     });
-
     xhr.open('POST', '/request');
-
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-    const data = `tag=${document.querySelector('.tag').value}`;
-
+    var data = "tag=".concat(document.querySelector('.tag').value);
     xhr.send(data);
   });
-
   document.querySelector('.tag').focus();
 });
-
-socket.on('gateway', function(open) {
+socket.on('gateway', function (open) {
   if (open == false) {
     switchmessages('have_a_great_day');
   } else {
