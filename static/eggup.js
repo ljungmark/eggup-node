1b32d62046a83aeb9660aceb12ff29fd92099e57
Mattias Ljungmark <mattias@ljungmark.co>
2020-02-07 15:31:08 +0100

    Q183

--- static/eggup.js
+++ static/eggup.js
@@ -1211,6 +1211,7 @@ Eggup.prototype.i18n = function(operation = 'get', pointer = null) {
             '180': '"Antingen så sjunker man, eller så lever man livet" -Ljungmark',
             '181': '"Det var en väldigt estetiskt tilltalande gurka" -Jimmy',
             '182': '"Precis som kungen, sån är jag!" -Therese',
+            '183': '"sdf" -Amanda',
           }
         },
         'map': {
