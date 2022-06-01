35d4d1dae13ff179f3f65137b5b93752555c2db8
Ljungmark <mattias@ljungmark.org>
2022-06-01 07:37:12 +0200

    Q189

--- static/eggup.js
+++ static/eggup.js
@@ -1217,6 +1217,7 @@ Eggup.prototype.i18n = function(operation = 'get', pointer = null) {
             '186': '"Why do we need QA? The answer is "We don\'t" - Gunnar',
             '187': '"Nu förtiden behöver man ju inte utsätta kollegor så mycket heller, så man kan göra riktigt galna saker som jag gjort idag genom att blanda Yatagan och Tresor Midnight Rose. Jag luktar som sexet efter något kungligt medeltida par: bäverkön, rök, hallon och rosor" -Henric',
             '188': '"Om ni blundar så kan jag visa Vedin vad som hände igår..." -Farre',
+            '189': '"En antik attraktion? Är det en GILF, eller?" -Algulin',
           }
         },
         'map': {
