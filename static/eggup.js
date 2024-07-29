b0cf72bd4b34aef8630f93780fe98d92878535c5
Ljungmark <mattias@ljungmark.org>
2024-07-29 19:20:25 +0200

    Q194

--- static/eggup.js
+++ static/eggup.js
@@ -1221,7 +1221,8 @@ Eggup.prototype.i18n = function(operation = 'get', pointer = null) {
             '190': '"My body have... substantially resized." -Petter',
             '191': '"Paymenthub är en jävla djungel asså" -Stewart',
             '192': '"Jag har blivit inspirerad av Webfront" -Qaniel',
-            '193': '"De viktiga personerna är här" -Admir "Jag sitter och målar! -Strålberg',
+            '193': '"De viktiga personerna är här" -Admir "Jag sitter och målar!" -Strålberg',
+            '194': 'Jag har gjort en massa dank shit nu som Patrik har fixat." -Joakim',
           }
         },
         'map': {
